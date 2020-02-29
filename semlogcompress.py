da57259b323753579c94b7c568a8ecac3af483bb
spio@tougener <spio@tougener>
2020-02-29 20:30:35 +0100

    - basic implementation of stream processing based on per-line reading

--- semlogcompress.py
+++ semlogcompress.py
@@ -17,16 +17,18 @@ colors = {
 def process(fn, verbose, limit_lines):
     if fn == "-":
         f = sys.stdin
+        hook = True
     else:
         f = open(fn)
+        hook = False
 
-    msgs = process_internal(f, verbose, limit_lines, fn)
+    msgs = process_internal(f, verbose, limit_lines, fn, hook)
 
     f.close()
 
     return msgs
 
-def process_internal(f, verbose, limit_lines, fnx):
+def process_internal(f, verbose, limit_lines, fnx, hook):
     msgs = {}
 
     i = 0
@@ -36,13 +38,13 @@ def process_internal(f, verbose, limit_lines, fnx):
             if limit_lines is not None and i > limit_lines:
                 break
 
-            process_line(line, msgs, verbose, i, fnx)
+            process_line(line, msgs, verbose, i, fnx, hook)
     except KeyboardInterrupt:
         print("(closing input)")
 
     return msgs
 
-def process_line(line, msgs, verbose, i, fnx):
+def process_line(line, msgs, verbose, i, fnx, hook):
     line = line.strip()
     #print(line)
     # m - month, d - day, t - time, ip - ipv4 address, dev - device; dt - date
@@ -67,6 +69,10 @@ def process_line(line, msgs, verbose, i, fnx):
             break
     if not clustered:
         msgs[r] = [r]
+        msg = r
+
+    if hook:
+        handle_msg(msgs, msg, verbose, 0)
 
 def typeget(s):
     if s.isdigit():
@@ -99,152 +105,8 @@ def handle(msgs, verbose):
         print("---")
     toh = 0
     for msg in sorted(msgs):
-        types = {}
-
-        if verbose:
-            print(colors["yellow"] + "X " + msg + " (cluster)" + colors["reset"])
-        alldiff = {}
-        exdiff = {}
-        for r in msgs[msg]:
-            if verbose:
-                print("*", r, "(raw log line)")
-            sm = difflib.SequenceMatcher(None, msg, r)
-            blocks = sm.get_matching_blocks()
-            if verbose:
-                print(" ", blocks)
-            cblocks = ""
-            lastb = None
-            diff = []
-            for block in blocks:
-                if lastb is not None and block.size != 0:
-                    cblocks += colors["red"] + r[lastb:block.b] + colors["reset"]
-                    diff.append((lastb, r[lastb:block.b]))
-                    exdiff[lastb] = exdiff.get(lastb, []) + [r[lastb:block.b]]
-                    if not lastb in alldiff or block.b - lastb > len(alldiff[lastb]):
-                        alldiff[lastb] = "*" * (block.b - lastb)
-                        types[lastb] = typeget(r[lastb:block.b])
-                cblocks += colors["green"] + r[block.b:block.b + block.size] + colors["reset"]
-                lastb = block.b + block.size
-            if verbose:
-                print("→", cblocks, "(diffed log line) (diff count D: " + str(len(diff)) + ")")
-                print("D", colors["magenta"] + str(diff) + colors["reset"])
-
-        dparts = sorted(alldiff.items())
-        if verbose:
-            print("#", dparts)
-        oh = printpatterns(dparts, r)
-
-        for j in range(len(dparts)):
-            if not dparts[len(dparts) - j - 1][1]:
-                continue
-            offset = 0
-            if verbose:
-                print(" " * 3, end="")
-            excerpt = []
-            for dpart in dparts[:len(dparts) - j]:
-                if not dpart[1]:
-                    continue
-                if verbose:
-                    print(" " * (dpart[0] - offset - 1) + "|", end="")
-                offset = dpart[0]
-            if verbose:
-                print("[" + str(types[dparts[len(dparts) - j - 1][0]]) + "] " + str(exdiff[dparts[len(dparts) - j - 1][0]][:5]))
-
-        dpartsext = dparts[:]
-        typesext = {}
-        #print("R", r, "T", types)
-        laststop = None
-        for j in range(len(dpartsext)):
-            if dparts[j][1]:
-                start = dparts[j][0]
-                stop = start + len(dparts[j][1]) - 1
-                t = types[dparts[j][0]]
-                #print(dparts[j], t, start, "..", stop)
-                estart = start
-                estop = stop
-                for k in range(1, 99):
-                    if (laststop and start - k == laststop) or start - k < 0:
-                        break
-                    if typeget(r[start - k]) == t:
-                        estart -= 1
-                    else:
-                        break
-                for k in range(1, 99):
-                    if (j < len(dpartsext) - 1 and stop + k == dparts[j + 1][0]) or stop + k == len(r):
-                        break
-                    if typeget(r[stop + k]) == t:
-                        estop += 1
-                    else:
-                        break
-                #print("==> extended type range", estart, estop)
-                dpartsext[j] = (estart, "*" * (estop - estart + 1))
-                typesext[estart] = types[start]
-                laststop = estop
-        if verbose:
-            print("#", dpartsext, "extended")
-        dpartsmer = []
-        for dpart in dpartsext:
-            dpartprev = None
-            if len(dpartsmer):
-                dpartprev = dpartsmer[-1]
-            if dpartprev and types[dpart[0]] == types[dpartprev[0]] and dpartprev[0] + len(dpartprev[1]) == dpart[0]:
-                dpartmer = (dpartprev[0], "*" * (len(dpartprev[1]) + len(dpart[1])))
-                #print("merge", dpartsmer[-1], dpart, "=>", dpartmer)
-                dpartsmer[-1] = dpartmer
-            else:
-                dpartsmer.append(dpart)
-        if verbose:
-            print("#", dpartsmer, "extended and merged")
-        printpatterns(dpartsmer, r, "(extended and merged)")
-
-        times = str(len(msgs[msg])) + " times"
-        toh += len(msg) + oh
-        if len(msg):
-            print("=", colors["cyan"] + "compression ratio " + str(round(100 * (len(msg) + oh) / (len(msg) * len(msgs[msg])))) + "% / " + times + colors["reset"])
-        else:
-            print("=", colors["cyan"] + "no compression ratio / empty line" + colors["reset"])
-
-        #retlist.append((msg, exdiff))
-        retlist.append((msg, msgs[msg]))
-
-        knowledge = {}
-        lastpos = 0
-        lastlen = 0
-        for dpart in dpartsmer:
-            if not len(dpart[1]):
-                continue
-            semtype = "unknown"
-            if types[dpart[0]] == "digit" and len(dpart[1]) in (4, 5):
-                semtype = "portnumber"
-            i = 1
-            semtypeorig = semtype
-            while semtype in knowledge:
-                i += 1
-                semtype = semtypeorig + str(i)
-            semre = ""
-            if semtypeorig == "portnumber":
-                semre = "(\\d{1," + str(len(dpart[1])) + "})"
-            else:
-                semre = "(.{1," + str(len(dpart[1])) + "})"
-            regex = msg[lastpos + lastlen:dpart[0] + 1] + semre
-            if verbose:
-                print(">", semtype, regex)
-            data = []
-            if verbose:
-                print(">>", lastpos, "..", dpart[0], regex)
-            for r in msgs[msg]:
-                sr = re.search(regex, r)
-                if sr:
-                    m = sr.group(1)
-                    if verbose:
-                        print("   M", m)
-                    data.append(m)
-            #for r in msgs[msg]:
-            #    data.append(r[dpart[0]:dpart[0] + len(dpart[1])])
-            knowledge[semtype] = data
-            lastpos = dpart[0]
-            lastlen = len(dpart[1]) + 1
-        print(">>> knowledge", knowledge)
+        retlist_line, toh = handle_msg(msgs, msg, verbose, toh)
+        retlist += retlist_line
 
     tlen = 0
     tlines = 0
@@ -255,6 +117,158 @@ def handle(msgs, verbose):
     print("TOTAL", tlines, "lines", tlen, "bytes → compressed", len(msgs), "lines", toh, "bytes", round(100 * toh / tlen, 2), "%")
 
     return retlist
+
+def handle_msg(msgs, msg, verbose, toh):
+    retlist = []
+
+    types = {}
+
+    if verbose:
+        print(colors["yellow"] + "X " + msg + " (cluster)" + colors["reset"])
+    alldiff = {}
+    exdiff = {}
+    for r in msgs[msg]:
+        if verbose:
+            print("*", r, "(raw log line)")
+        sm = difflib.SequenceMatcher(None, msg, r)
+        blocks = sm.get_matching_blocks()
+        if verbose:
+            print(" ", blocks)
+        cblocks = ""
+        lastb = None
+        diff = []
+        for block in blocks:
+            if lastb is not None and block.size != 0:
+                cblocks += colors["red"] + r[lastb:block.b] + colors["reset"]
+                diff.append((lastb, r[lastb:block.b]))
+                exdiff[lastb] = exdiff.get(lastb, []) + [r[lastb:block.b]]
+                if not lastb in alldiff or block.b - lastb > len(alldiff[lastb]):
+                    alldiff[lastb] = "*" * (block.b - lastb)
+                    types[lastb] = typeget(r[lastb:block.b])
+            cblocks += colors["green"] + r[block.b:block.b + block.size] + colors["reset"]
+            lastb = block.b + block.size
+        if verbose:
+            print("→", cblocks, "(diffed log line) (diff count D: " + str(len(diff)) + ")")
+            print("D", colors["magenta"] + str(diff) + colors["reset"])
+
+    dparts = sorted(alldiff.items())
+    if verbose:
+        print("#", dparts)
+    oh = printpatterns(dparts, r)
+
+    for j in range(len(dparts)):
+        if not dparts[len(dparts) - j - 1][1]:
+            continue
+        offset = 0
+        if verbose:
+            print(" " * 3, end="")
+        excerpt = []
+        for dpart in dparts[:len(dparts) - j]:
+            if not dpart[1]:
+                continue
+            if verbose:
+                print(" " * (dpart[0] - offset - 1) + "|", end="")
+            offset = dpart[0]
+        if verbose:
+            print("[" + str(types[dparts[len(dparts) - j - 1][0]]) + "] " + str(exdiff[dparts[len(dparts) - j - 1][0]][:5]))
+
+    dpartsext = dparts[:]
+    typesext = {}
+    #print("R", r, "T", types)
+    laststop = None
+    for j in range(len(dpartsext)):
+        if dparts[j][1]:
+            start = dparts[j][0]
+            stop = start + len(dparts[j][1]) - 1
+            t = types[dparts[j][0]]
+            #print(dparts[j], t, start, "..", stop)
+            estart = start
+            estop = stop
+            for k in range(1, 99):
+                if (laststop and start - k == laststop) or start - k < 0:
+                    break
+                if typeget(r[start - k]) == t:
+                    estart -= 1
+                else:
+                    break
+            for k in range(1, 99):
+                if (j < len(dpartsext) - 1 and stop + k == dparts[j + 1][0]) or stop + k == len(r):
+                    break
+                if typeget(r[stop + k]) == t:
+                    estop += 1
+                else:
+                    break
+            #print("==> extended type range", estart, estop)
+            dpartsext[j] = (estart, "*" * (estop - estart + 1))
+            typesext[estart] = types[start]
+            laststop = estop
+    if verbose:
+        print("#", dpartsext, "extended")
+    dpartsmer = []
+    for dpart in dpartsext:
+        dpartprev = None
+        if len(dpartsmer):
+            dpartprev = dpartsmer[-1]
+        if dpartprev and types[dpart[0]] == types[dpartprev[0]] and dpartprev[0] + len(dpartprev[1]) == dpart[0]:
+            dpartmer = (dpartprev[0], "*" * (len(dpartprev[1]) + len(dpart[1])))
+            #print("merge", dpartsmer[-1], dpart, "=>", dpartmer)
+            dpartsmer[-1] = dpartmer
+        else:
+            dpartsmer.append(dpart)
+    if verbose:
+        print("#", dpartsmer, "extended and merged")
+    printpatterns(dpartsmer, r, "(extended and merged)")
+
+    times = str(len(msgs[msg])) + " times"
+    toh += len(msg) + oh
+    if len(msg):
+        print("=", colors["cyan"] + "compression ratio " + str(round(100 * (len(msg) + oh) / (len(msg) * len(msgs[msg])))) + "% / " + times + colors["reset"])
+    else:
+        print("=", colors["cyan"] + "no compression ratio / empty line" + colors["reset"])
+
+    #retlist.append((msg, exdiff))
+    retlist.append((msg, msgs[msg]))
+
+    knowledge = {}
+    lastpos = 0
+    lastlen = 0
+    for dpart in dpartsmer:
+        if not len(dpart[1]):
+            continue
+        semtype = "unknown"
+        if types[dpart[0]] == "digit" and len(dpart[1]) in (4, 5):
+            semtype = "portnumber"
+        i = 1
+        semtypeorig = semtype
+        while semtype in knowledge:
+            i += 1
+            semtype = semtypeorig + str(i)
+        semre = ""
+        if semtypeorig == "portnumber":
+            semre = "(\\d{1," + str(len(dpart[1])) + "})"
+        else:
+            semre = "(.{1," + str(len(dpart[1])) + "})"
+        regex = msg[lastpos + lastlen:dpart[0] + 1] + semre
+        if verbose:
+            print(">", semtype, regex)
+        data = []
+        if verbose:
+            print(">>", lastpos, "..", dpart[0], regex)
+        for r in msgs[msg]:
+            sr = re.search(regex, r)
+            if sr:
+                m = sr.group(1)
+                if verbose:
+                    print("   M", m)
+                data.append(m)
+        #for r in msgs[msg]:
+        #    data.append(r[dpart[0]:dpart[0] + len(dpart[1])])
+        knowledge[semtype] = data
+        lastpos = dpart[0]
+        lastlen = len(dpart[1]) + 1
+    print(">>> knowledge", knowledge)
+
+    return retlist, toh
 
 def persist(retlist):
     #print(retlist)
